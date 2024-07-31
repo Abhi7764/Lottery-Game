2251497d6f7c9ca53d1eb2e0fac712994d95b6e6
Abhishek Gupta <abhishekguptadd26@gmail.com>
2024-07-31 22:33:45 +0530

    Initial File

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Lottery from './Lottery'
+import { sum } from './helper';
+
+function App() {
+
+  let winCondition = (ticket) => {
+    // return sum(ticket) === 15;
+    return ticket.every((num)=>num ===ticket[0]);
+    //return ticket[0] === 0;
+  }
+  return (
+    <>
+      <Lottery n={3} winCondition={winCondition}/>
+    </>
+  )
+}
+
+export default App
